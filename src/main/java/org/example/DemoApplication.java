ded9346ae73ee9391541da2608969e9bafb03065
ThanhThang5722 <23521427@gm.uit.edu.vn>
2025-11-05 14:37:50 +0700

    (feature): Update remaining days every year

--- src/main/java/org/example/DemoApplication.java
+++ src/main/java/org/example/DemoApplication.java
@@ -1,19 +1,36 @@
 package org.example;
 
+import org.example.service.LeaveBalanceService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    private final LeaveBalanceService leaveBalanceService;
+
+    public DemoApplication(LeaveBalanceService leaveBalanceService) {
+        this.leaveBalanceService = leaveBalanceService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello World");
+    }
+
+    //@Scheduled(fixedRate = 600000)  // Chạy mỗi 5 giây
+    @Scheduled(cron = "0 0 0 1 1 *")
+    public void incrementRemainingDays() {
+        leaveBalanceService.incrementAllRemainingDays();
+        System.out.println("Incremented remaining days for all users at " + java.time.LocalDateTime.now());
     }
 }
